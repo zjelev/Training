17d38bf75342f6fcad5bc044d20676b99877b1de
zjelev <zlatomir@gmail.com>
2024-01-01 11:34:20 +0200

    Added windows user authentication

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,10 +1,22 @@
 using BlazorApp.Components;
+using Microsoft.AspNetCore.Authentication.Negotiate;													
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
